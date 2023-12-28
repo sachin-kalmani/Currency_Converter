4909d3464aeceda1d69490f9a6c323af7e9f5beb
sachin-kalmani <64184686+sachin-kalmani@users.noreply.github.com>
2023-12-28 19:54:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let api=`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_fMaVt1lC70nE5KRcGQ1dw7ea4QDOd4Ku8Zly4jHn`;   
+
+
+const fromDropDown=document.getElementById("from-currency-select");
+const toDropDown=document.getElementById("to-currency-select");
+const result=document.getElementById("result");
+currencies.forEach((currency)=>{
+    const option=document.createElement("option");
+    option.value=currency;
+    option.text=currency;
+
+    fromDropDown.add(option);
+});
+
+currencies.forEach((currency)=>{
+    const option=document.createElement("option");
+    option.value=currency;
+    option.text=currency;
+    toDropDown.add(option);
+});
+
+let convertCurrency=()=>{
+   const amount=document.querySelector("#amount").value;
+   const fromCurrency=fromDropDown.value;
+   const toCurrency=toDropDown.value;
+    var xhr=new XMLHttpRequest();
+    xhr.open("GET",api,true);
+    xhr.send()
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState==4)
+        {
+            var jsonData=JSON.parse(xhr.responseText);
+            let fromExchangeRate=jsonData.data[fromCurrency];
+            let toExchangeRate=jsonData.data[toCurrency];
+            const convertedAmount=(amount/fromExchangeRate)*toExchangeRate;
+            result.innerHTML=`${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
+        }
+    }
+};
+fromDropDown.addEventListener("click",convertCurrency);
+toDropDown.addEventListener("click",convertCurrency);
+document.querySelector("#convert-button").addEventListener("click",convertCurrency);
+window.addEventListener("load",convertCurrency);
